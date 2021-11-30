85b86dce0c1ef5401f880961a0855366ebf03d82
chengen0612 <chengen0612@gmail.com>
2021-11-30 21:24:42 +0800

    log: switch basis to react style

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-const greeting = () => {
-  console.log("hello webpack!");
-};
+import React from "react";
+import ReactDOM from "react-dom";
 
-greeting();
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
